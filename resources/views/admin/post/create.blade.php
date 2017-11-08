5aaecb10b67d1dbb4f3cbbfb8f4120e2e5db7837
Pedro Rojas <projas@ltegroup.com.mx>
2017-11-08 09:21:57 -0600

    vista para crear layout, formulario para crear post, generar controller de post

---
+++ resources/views/admin/post/create.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('content')
+	<div class="panel panel-default">
+		<div class="panel-heading">
+			Nuevo post
+		</div>
+		<div class="panel-body">
+			<form action="/post/store" method="post">
+				{{ csrf_field() }}
+
+				<div class="form-group">
+					<label for="title">Titulo</label>
+					<input type="text" name="title" class="form-control">
+				</div>
+				<div class="form-group">
+					<label for="featured">Featured image</label>
+					<input type="file" name="featured" class="form-control">	
+				</div>
+				<div class="form-group">
+					<label for="content">Contenido</label>
+					<textarea name="content" id="content" cols="5" rows="5" class="form-control"></textarea>
+				</div>
+				<div class="form-group">
+					<div class="text-center">
+						<button class="btn btn-success" type="submit">Guardar post</button>
+					</div>
+				</div>
+
+			</form>
+		</div>
+	</div>
+@stop
