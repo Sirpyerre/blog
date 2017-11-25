6d6746b192a0cfbf466c734523b04d4dec8292fe
Pedro Rojas <projas@ltegroup.com.mx>
2017-11-25 12:01:13 -0600

    crear modelo, controller settings, tabla de migración settings y seeder

---
+++ app/Http/Controllers/SettingsController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Setting;
+
+class SettingsController extends Controller
+{
+    public function update()
+    {
+    	$setting = Setting::first();
+    }
+}
